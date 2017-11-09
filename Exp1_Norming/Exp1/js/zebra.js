fcd5edfe9c5dd662d1183ca25f1cc735d3c3e961
loey18 <loey@u.rochester.edu>
2017-11-09 15:46:48 -0500

    Fixed sampling without replacement function

--- Exp1_Norming/Exp1/js/zebra.js
+++ Exp1_Norming/Exp1/js/zebra.js
@@ -16,7 +16,6 @@ function make_slides(f) {
       var instr_sample_order = sample_without_replacement(instr_sample_size);
       for(var i=0; i<instr_sample_size; i++){
         $('#img_example'+i).attr("src", "img/" + instr_sample_order[i]);
-        console.log(instr_sample_order[i]);
       }
     },
 
@@ -25,8 +24,6 @@ function make_slides(f) {
     }
   });
 
-
-
   slides.one_slider = slide({
     name : "one_slider",
 
@@ -34,23 +31,16 @@ function make_slides(f) {
      (the variable 'stim' will change between each of these values,
       and for each of these, present_handle will be run.) */
 
-    present : [
-      {image: "stim_whiteFill_v12x112.5y250_v23x112.5y168.75_v13x50y181.25.png"},
-      {image: "stim_whiteFill_v12x106.25y175_v23x100y131.25_v13x43.75y143.75.png"},
-      {image: "stim_whiteFill_v12x137.5y175_v23x137.5y131.25_v13x100y143.75.png"},
-      {image: "stim_whiteFill_v12x112.5y137.5_v23x112.5y112.5_v13x50y125.png"},
-      {image: "stim_whiteFill_v12x112.5y181.25_v23x112.5y143.75_v13x50y150.png"},
-      
-    ],
+    present: sample_without_replacement(stimuli_images.length),
 
     //this gets run only at the beginning of the block
     present_handle : function(stim) {
       $(".err").hide();
 
       this.stim = stim; //I like to store this information in the slide so I can record it later.
-      //$("#column2").append("<img class=\"img_prototype\" src=\"../../Stimuli/stimuli_png/stim_whiteFill_v12x112.5y187.5_v23x112.5y137.5_v13x50y150.png\" height=250\">");
-      $("#img_comparison").attr("src", "img/" + stim.image);
-      //$("#img_comparison").src.replace("file", "stim_blackFill_v12x112.5y137.5_v23x112.5y112.5_v13x50y125");
+      
+      $("#img_comparison").attr("src", "img/" + stim);
+
       this.init_sliders();
       exp.sliderPost = null; //erase current slider value
     },
@@ -160,62 +150,111 @@ function init() {
 }
 
 var stimuli_images = [
-  "stim_whiteFill_v12x112.5y250_v23x112.5y168.75_v13x50y181.25.png",
-  "stim_whiteFill_v12x106.25y175_v23x100y131.25_v13x43.75y143.75.png",
-  "stim_whiteFill_v12x137.5y175_v23x137.5y131.25_v13x100y143.75.png",
-  "stim_whiteFill_v12x112.5y137.5_v23x112.5y112.5_v13x50y125.png",
-  "stim_whiteFill_v12x112.5y181.25_v23x112.5y143.75_v13x50y150.png",
-  "stim_whiteFill_v12x112.5y162.5_v23x112.5y125_v13x50y137.5.png",
-  "stim_whiteFill_v12x112.5y150_v23x112.5y106.25_v13x50y93.75.png",
-  "stim_whiteFill_v12x112.5y212.5_v23x112.5y168.75_v13x50y218.75.png",
-  "stim_whiteFill_v12x100y175_v23x87.5y131.25_v13x37.5y143.75.png",
-  "stim_whiteFill_v12x93.75y175_v23x75y131.25_v13x131.25y143.75.png",
-  "stim_whiteFill_v12x125y175_v23x137.5y131.25_v13x62.5y143.75.png",
-  "stim_whiteFill_v12x112.5y137.5_v23x112.5y93.75_v13x50y68.75.png",
-  "stim_whiteFill_v12x87.5y175_v23x100y131.25_v13x37.5y143.75.png",
-  "stim_whiteFill_v12x112.5y150_v23x112.5y118.75_v13x50y131.25.png",
-  "stim_whiteFill_v12x125y175_v23x125y131.25_v13x75y143.75.png",
-  "stim_whiteFill_v12x112.5y156.25_v23x112.5y112.5_v13x50y106.25.png",
-  "stim_whiteFill_v12x125y175_v23x118.75y131.25_v13x56.25y143.75.png",
-  "stim_whiteFill_v12x112.5y131.25_v23x112.5y87.5_v13x50y56.25.png",
-  "stim_whiteFill_v12x112.5y212.5_v23x112.5y150_v13x50y162.5.png",
-  "stim_whiteFill_v12x118.75y175_v23x125y131.25_v13x56.25y143.75.png",
-  "stim_whiteFill_v12x112.5y206.25_v23x112.5y162.5_v13x50y206.25.png",
   "stim_whiteFill_v12x75y175_v23x93.75y131.25_v13x31.25y143.75.png",
-  "stim_whiteFill_v12x112.5y218.75_v23x112.5y175_v13x50y231.25.png",
-  "stim_whiteFill_v12x118.75y175_v23x118.75y131.25_v13x62.5y143.75.png",
-  "stim_whiteFill_v12x112.5y200_v23x112.5y156.25_v13x50y193.75.png",
-  "stim_whiteFill_v12x93.75y175_v23x93.75y131.25_v13x12.5y143.75.png",
-  "stim_whiteFill_v12x131.25y175_v23x131.25y131.25_v13x87.5y143.75.png",
-  "stim_whiteFill_v12x112.5y237.5_v23x112.5y162.5_v13x50y175.png",
-  "stim_whiteFill_v12x150y175_v23x150y131.25_v13x125y143.75.png",
-  "stim_whiteFill_v12x106.25y175_v23x106.25y131.25_v13x37.5y143.75.png",
-  "stim_whiteFill_v12x112.5y168.75_v23x112.5y118.75_v13x50y137.5.png",
-  "stim_whiteFill_v12x112.5y187.5_v23x112.5y156.25_v13x50y156.25.png",
-  "stim_whiteFill_v12x100y175_v23x106.25y131.25_v13x43.75y143.75.png",
   "stim_whiteFill_v12x162.5y175_v23x162.5y131.25_v13x150y143.75.png",
   "stim_whiteFill_v12x112.5y162.5_v23x112.5y106.25_v13x50y131.25.png",
-  "stim_whiteFill_v12x112.5y193.75_v23x112.5y150_v13x50y181.25.png",
-  "stim_whiteFill_v12x112.5y225_v23x112.5y156.25_v13x50y168.75.png",
-  "stim_whiteFill_v12x112.5y143.75_v23x112.5y100_v13x50y81.25.png",
-  "stim_whiteFill_v12x112.5y200_v23x112.5y143.75_v13x50y156.25.png",
-  "stim_whiteFill_v12x143.75y175_v23x143.75y131.25_v13x112.5y143.75.png",
-  "stim_whiteFill_v12x112.5y187.5_v23x112.5y137.5_v13x50y150.png",
-  "stim_whiteFill_v12x112.5y181.25_v23x112.5y137.5_v13x50y156.25.png",
-  "stim_whiteFill_v12x112.5y187.5_v23x112.5y143.75_v13x50y168.75.png",
   "stim_whiteFill_v12x112.5y175_v23x112.5y131.25_v13x50y143.75.png",
-  "stim_whiteFill_v12x137.5y175_v23x125y131.25_v13x62.5y143.75.png",
-  "stim_whiteFill_v12x156.25y175_v23x156.25y131.25_v13x137.5y143.75.png",
-  "stim_whiteFill_v12x112.5y162.5_v23x112.5y118.75_v13x50y118.75.png",
+  "stim_blackFill_v12x131.25y175_v23x131.25y131.25_v13x87.5y143.75.png",
+  "stim_whiteFill_v12x112.5y137.5_v23x112.5y112.5_v13x50y125.png",
+  "stim_whiteFill_v12x112.5y181.25_v23x112.5y137.5_v13x50y156.25.png",
+  "stim_blackFill_v12x100y175_v23x87.5y131.25_v13x37.5y143.75.png",
+  "stim_whiteFill_v12x112.5y137.5_v23x112.5y93.75_v13x50y68.75.png",
+  "stim_whiteFill_v12x100y175_v23x100y131.25_v13x25y143.75.png",
+  "stim_blackFill_v12x125y175_v23x137.5y131.25_v13x62.5y143.75.png",
   "stim_whiteFill_v12x112.5y168.75_v23x112.5y125_v13x50y131.25.png",
-  "stim_whiteFill_v12x100y175_v23x100y131.25_v13x25y143.75.png"
+  "stim_blackFill_v12x162.5y175_v23x162.5y131.25_v13x150y143.75.png",
+  "stim_whiteFill_v12x112.5y150_v23x112.5y118.75_v13x50y131.25.png",
+  "stim_whiteFill_v12x112.5y187.5_v23x112.5y137.5_v13x50y150.png",
+  "stim_blackFill_v12x112.5y212.5_v23x112.5y150_v13x50y162.5.png",
+  "stim_blackFill_v12x112.5y137.5_v23x112.5y112.5_v13x50y125.png",
+  "stim_blackFill_v12x112.5y212.5_v23x112.5y168.75_v13x50y218.75.png",
+  "stim_whiteFill_v12x112.5y225_v23x112.5y156.25_v13x50y168.75.png",
+  "stim_blackFill_v12x112.5y218.75_v23x112.5y175_v13x50y231.25.png",
+  "stim_whiteFill_v12x112.5y131.25_v23x112.5y87.5_v13x50y56.25.png",
+  "stim_whiteFill_v12x112.5y187.5_v23x112.5y156.25_v13x50y156.25.png",
+  "stim_blackFill_v12x125y175_v23x125y131.25_v13x75y143.75.png",
+  "stim_blackFill_v12x137.5y175_v23x125y131.25_v13x62.5y143.75.png",
+  "stim_whiteFill_v12x131.25y175_v23x131.25y131.25_v13x87.5y143.75.png",
+  "stim_blackFill_v12x93.75y175_v23x75y131.25_v13x131.25y143.75.png",
+  "stim_whiteFill_v12x100y175_v23x106.25y131.25_v13x43.75y143.75.png",
+  "stim_whiteFill_v12x112.5y181.25_v23x112.5y143.75_v13x50y150.png",
+  "stim_blackFill_v12x112.5y187.5_v23x112.5y143.75_v13x50y168.75.png",
+  "stim_blackFill_v12x112.5y162.5_v23x112.5y118.75_v13x50y118.75.png",
+  "stim_blackFill_v12x100y175_v23x106.25y131.25_v13x43.75y143.75.png",
+  "stim_whiteFill_v12x93.75y175_v23x75y131.25_v13x131.25y143.75.png",
+  "stim_blackFill_v12x106.25y175_v23x106.25y131.25_v13x37.5y143.75.png",
+  "stim_blackFill_v12x112.5y181.25_v23x112.5y143.75_v13x50y150.png",
+  "stim_whiteFill_v12x143.75y175_v23x143.75y131.25_v13x112.5y143.75.png",
+  "stim_whiteFill_v12x137.5y175_v23x125y131.25_v13x62.5y143.75.png",
+  "stim_whiteFill_v12x125y175_v23x137.5y131.25_v13x62.5y143.75.png",
+  "stim_blackFill_v12x112.5y131.25_v23x112.5y87.5_v13x50y56.25.png",
+  "stim_blackFill_v12x112.5y162.5_v23x112.5y125_v13x50y137.5.png",
+  "stim_whiteFill_v12x112.5y193.75_v23x112.5y150_v13x50y181.25.png",
+  "stim_whiteFill_v12x112.5y250_v23x112.5y168.75_v13x50y181.25.png",
+  "stim_blackFill_v12x118.75y175_v23x125y131.25_v13x56.25y143.75.png",
+  "stim_whiteFill_v12x137.5y175_v23x137.5y131.25_v13x100y143.75.png",
+  "stim_blackFill_v12x112.5y250_v23x112.5y168.75_v13x50y181.25.png",
+  "stim_blackFill_v12x112.5y143.75_v23x112.5y100_v13x50y81.25.png",
+  "stim_blackFill_v12x112.5y162.5_v23x112.5y106.25_v13x50y131.25.png",
+  "stim_blackFill_v12x112.5y150_v23x112.5y118.75_v13x50y131.25.png",
+  "stim_whiteFill_v12x112.5y162.5_v23x112.5y118.75_v13x50y118.75.png",
+  "stim_blackFill_v12x143.75y175_v23x143.75y131.25_v13x112.5y143.75.png",
+  "stim_blackFill_v12x125y175_v23x118.75y131.25_v13x56.25y143.75.png",
+  "stim_blackFill_v12x112.5y187.5_v23x112.5y137.5_v13x50y150.png",
+  "stim_blackFill_v12x112.5y237.5_v23x112.5y162.5_v13x50y175.png",
+  "stim_whiteFill_v12x112.5y162.5_v23x112.5y125_v13x50y137.5.png",
+  "stim_blackFill_v12x150y175_v23x150y131.25_v13x125y143.75.png",
+  "stim_blackFill_v12x137.5y175_v23x137.5y131.25_v13x100y143.75.png",
+  "stim_whiteFill_v12x112.5y143.75_v23x112.5y100_v13x50y81.25.png",
+  "stim_whiteFill_v12x118.75y175_v23x118.75y131.25_v13x62.5y143.75.png",
+  "stim_whiteFill_v12x125y175_v23x125y131.25_v13x75y143.75.png",
+  "stim_blackFill_v12x112.5y168.75_v23x112.5y118.75_v13x50y137.5.png",
+  "stim_whiteFill_v12x112.5y200_v23x112.5y143.75_v13x50y156.25.png",
+  "stim_whiteFill_v12x112.5y150_v23x112.5y106.25_v13x50y93.75.png",
+  "stim_blackFill_v12x112.5y137.5_v23x112.5y93.75_v13x50y68.75.png",
+  "stim_blackFill_v12x75y175_v23x93.75y131.25_v13x31.25y143.75.png",
+  "stim_whiteFill_v12x118.75y175_v23x125y131.25_v13x56.25y143.75.png",
+  "stim_blackFill_v12x112.5y187.5_v23x112.5y156.25_v13x50y156.25.png",
+  "stim_whiteFill_v12x112.5y218.75_v23x112.5y175_v13x50y231.25.png",
+  "stim_whiteFill_v12x112.5y200_v23x112.5y156.25_v13x50y193.75.png",
+  "stim_blackFill_v12x112.5y200_v23x112.5y143.75_v13x50y156.25.png",
+  "stim_whiteFill_v12x112.5y212.5_v23x112.5y168.75_v13x50y218.75.png",
+  "stim_blackFill_v12x93.75y175_v23x93.75y131.25_v13x12.5y143.75.png",
+  "stim_blackFill_v12x106.25y175_v23x100y131.25_v13x43.75y143.75.png",
+  "stim_blackFill_v12x87.5y175_v23x100y131.25_v13x37.5y143.75.png",
+  "stim_whiteFill_v12x100y175_v23x87.5y131.25_v13x37.5y143.75.png",
+  "stim_whiteFill_v12x93.75y175_v23x93.75y131.25_v13x12.5y143.75.png",
+  "stim_whiteFill_v12x112.5y156.25_v23x112.5y112.5_v13x50y106.25.png",
+  "stim_blackFill_v12x112.5y168.75_v23x112.5y125_v13x50y131.25.png",
+  "stim_whiteFill_v12x106.25y175_v23x106.25y131.25_v13x37.5y143.75.png",
+  "stim_blackFill_v12x112.5y193.75_v23x112.5y150_v13x50y181.25.png",
+  "stim_whiteFill_v12x112.5y206.25_v23x112.5y162.5_v13x50y206.25.png",
+  "stim_blackFill_v12x118.75y175_v23x118.75y131.25_v13x62.5y143.75.png",
+  "stim_blackFill_v12x112.5y175_v23x112.5y131.25_v13x50y143.75.png",
+  "stim_blackFill_v12x156.25y175_v23x156.25y131.25_v13x137.5y143.75.png",
+  "stim_blackFill_v12x112.5y150_v23x112.5y106.25_v13x50y93.75.png",
+  "stim_blackFill_v12x112.5y200_v23x112.5y156.25_v13x50y193.75.png",
+  "stim_blackFill_v12x112.5y206.25_v23x112.5y162.5_v13x50y206.25.png",
+  "stim_whiteFill_v12x112.5y212.5_v23x112.5y150_v13x50y162.5.png",
+  "stim_whiteFill_v12x156.25y175_v23x156.25y131.25_v13x137.5y143.75.png",
+  "stim_whiteFill_v12x112.5y168.75_v23x112.5y118.75_v13x50y137.5.png",
+  "stim_whiteFill_v12x87.5y175_v23x100y131.25_v13x37.5y143.75.png",
+  "stim_whiteFill_v12x112.5y237.5_v23x112.5y162.5_v13x50y175.png",
+  "stim_blackFill_v12x100y175_v23x100y131.25_v13x25y143.75.png",
+  "stim_blackFill_v12x112.5y181.25_v23x112.5y137.5_v13x50y156.25.png",
+  "stim_whiteFill_v12x112.5y187.5_v23x112.5y143.75_v13x50y168.75.png",
+  "stim_blackFill_v12x112.5y225_v23x112.5y156.25_v13x50y168.75.png",
+  "stim_whiteFill_v12x150y175_v23x150y131.25_v13x125y143.75.png",
+  "stim_whiteFill_v12x106.25y175_v23x100y131.25_v13x43.75y143.75.png",
+  "stim_blackFill_v12x112.5y156.25_v23x112.5y112.5_v13x50y106.25.png",
+  "stim_whiteFill_v12x125y175_v23x118.75y131.25_v13x56.25y143.75.png",
 ]
 
 function sample_without_replacement(sampleSize){
-  var urn = stimuli_images;
+  var urn = stimuli_images.slice(0);
   var return_sample = [];
   for(var i=0; i<sampleSize; i++){
-    var randomIndex = Math.floor(Math.random()*stimuli_images.length);
+    var randomIndex = Math.floor(Math.random()*urn.length);
     return_sample.push(urn.splice(randomIndex, 1)[0]);
   }
   return return_sample;
